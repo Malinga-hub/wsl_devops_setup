982638c701d1af985e14071f3600606fdac501eb
malinga_hub <malinga.simuchimba@gmail.com>
2025-01-10 11:55:17 +0200

    microk8s script update

--- ubuntu_wsl/microk8s_install.sh
+++ ubuntu_wsl/microk8s_install.sh
@@ -5,14 +5,7 @@ sudo snap install microk8s --classic
 
 echo "adding microk8s user and group..."
 sudo usermod -aG microk8s $USER
-sudo chown -R $USER ~/.kube
 newgrp microk8s
-
-echo "enabling microk8s as service..."
-sudo systemctl enable snap.microk8s.daemon.service
-sudo systemctl start snap.microk8s.daemon.service
-sudo systemctl status snap.microk8s.daemon.service
-
 
 #optional
 #set alias=microk8s kubectl -> ~/.bashrc -> source ~/.bashrc -> to save settings
