028a5f1ff8b79ca4a3e8a4e3908dbe1ba35064ac
malinga-hub <malinga.simuchimba@gmail.com>
2025-03-01 16:33:51 +0200

    test script - 2

--- ubuntu_wsl/install/install_kafka.sh
+++ ubuntu_wsl/install/install_kafka.sh
@@ -18,23 +18,11 @@ ls -ltra
 
 echo "Configuring Kafka to run as a service..."
 
-sudo tee /opt/kafka/kafka_2.13-3.9.0/kafka-service-start-script.sh > /dev/null << 'EOF'
-#!/bin/bash
-nohup ./bin/zookeeper-server-start.sh ./config/zookeeper.properties >> /var/log/zookeeper.log 2>&1
-sleep 10
-nohup ./bin/kafka-server-start.sh ./config/server.properties >> /var/log/kafka-server.log 2>&1
+sudo tee ~/.bashrc > /dev/null << 'EOF'
+export KAFKA_HOME="/opt/kafka_2.13-3.9.0/"
 EOF
-
-sudo tee /opt/kafka/kafka_2.13-3.9.0/kafka-service-stop-script.sh > /dev/null << 'EOF'
-#!/bin/bash
-nohup ./bin/kafka-server-stop.sh >> /var/log/zookeeper.log 2>&1
-sleep 10
-nohup ./bin/zookeeper-server-stop.sh >> /var/log/kafka-server.log 2>&1
-rm -rf /tmp/kafka-logs /tmp/zookeeper /tmp/kraft-combined-logs
-EOF
-
-#make executable
-sudo chmod +x /opt/kafka/kafka_2.13-3.9.0/*.sh
+source ~/.bashrc
+echo "added KAKFA_HOME : $KAFKA_HOME to user profile"
 
 # Write the service file using a here-document with sudo tee.
 sudo tee /etc/systemd/system/kafka.service > /dev/null << 'EOF'
@@ -47,8 +35,11 @@ User=root
 Group=root
 
 Restart=always
-ExecStart=/opt/kafka/kafka_2.13-3.9.0/kafka-service-start-script.sh
-ExecStopPost=/opt/kafka/kafka_2.13-3.9.0/kafka-service-stop-script.sh
+ExecStartPre=$KAFKA_HOME/bin/zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.properties
+ExecStartPre=/bin/sleep 10
+ExecStart=$KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties
+ExecStopPost=$KAFKA_HOME/bin/kafka-server-start.sh
+ExecStopPost=$KAFKA_HOME/bin/zookeeper-server-start.sh
 
 [Install]
 WantedBy=multi-user.target
@@ -57,7 +48,7 @@ EOF
 # Reload systemd, enable and start the Kafka service.
 echo "reloading daemon services.."
 sudo systemctl daemon-reload
-echo "starting kafka service.."
+echo "starting kafka service on port 9092.."
 sudo systemctl start kafka.service
 echo "enabling kafka service.."
 sudo systemctl enable kafka.service
