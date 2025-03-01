17c2a177e9a35606be1e2f0e5808fd8243996981
malinga-hub <malinga.simuchimba@gmail.com>
2025-03-01 19:06:13 +0200

    kafka server retry - 4

--- ubuntu_wsl/install/kafka_install.sh
+++ ubuntu_wsl/install/kafka_install.sh
@@ -39,7 +39,7 @@ sudo tee /opt/kafka/kafka_2.13-3.9.0/kafka-stop-script.sh > /dev/null << EOF
 nohup $KAFKA_HOME/bin/kafka-server-stop.sh &
 sleep 10
 nohup $KAFKA_HOME/bin/zookeeper-server-stop.sh &
-sleep 10
+rm -rf /tmp/kafka-logs /tmp/zookeeper
 EOF
 
 chmod +x /opt/kafka/kafka_2.13-3.9.0/*.sh
