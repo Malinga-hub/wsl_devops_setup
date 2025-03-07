299512872a8ab9f9725bdac52f130c8e23f02e22
malinga-hub <malinga.simuchimba@gmail.com>
2025-03-07 16:02:01 +0200

    kafka scripts - kafka storage format - test 2

--- ubuntu_wsl/install/kafka_install.sh
+++ ubuntu_wsl/install/kafka_install.sh
@@ -32,9 +32,8 @@ KAFKA_CLUSTER_ID="$(/opt/kafka/kafka_2.13-3.9.0/bin/kafka-storage.sh random-uuid
 sudo tee /opt/kafka/kafka_2.13-3.9.0/kafka-start-script.sh > /dev/null << EOF
 #!/bin/bash
 echo "KAFKA_CLUSTER_ID: $KAFKA_CLUSTER_ID"
-sleep 5
 nohup $KAFKA_HOME/bin/kafka-storage.sh format --standalone -t $KAFKA_CLUSTER_ID -c $KAFKA_HOME/config/kraft/reconfig-server.properties &
-sleep 5
+sleep 10
 nohup $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/kraft/reconfig-server.properties &
 EOF
 
